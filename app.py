20875986f7517afe98edf1054df086e3fa239154
NJOROO <khalebptah@outlook.com>
2022-07-06 16:09:13 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template,request,flash
+import pickle
+import os
+
+app=Flask(__name__)
+app.config["SECRET_KEY"]="6666uyffft"
+Model = pickle.load(open('model.pkl','rb'))
+
+@app.route("/",methods=["POST","GET"])	
+@app.route("/predict",methods=['POST','GET'])
+def predict():
+	if request.method=="POST":
+		mileage=int(request.form.get("mileage"))
+		location=int(request.form.get('location'))
+		year=int(request.form.get("Year"))
+		transmission=int(request.form.get("Transmission"))
+		condition=int(request.form.get("Condition"))
+		fuel_type=int(request.form.get("FuelType"))
+		make=int(request.form.get("Make"))
+		model=int(request.form.get("Model"))
+		data=[[model,year,condition,transmission,fuel_type,location,mileage,make]]
+		prediction=Model.predict(data)[0]
+		output=round(prediction,-3)
+		output=format(output,",")
+		flash(f"The car is worth about {output} Kenyan Shillings","success")
+	return render_template("index.html")
+	
+if __name__=="__main__":
+	app.run()
